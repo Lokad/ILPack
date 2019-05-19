3f4a5af632d4cccea5c9c403f76cd60be982d9e3
Brad Robinson <contact@toptensoftware.com>
2019-05-19 12:45:41 +1000

    Renamed 'RewriteOriginal' project to 'TestSubject'

--- test/TestSubject/MyClass.cs
+++ test/TestSubject/MyClass.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
 // This project defines a set of types that will be rewritten by the test cases to a new
-// dll named "RewriteClone".  The test cases then compare the final type information of both
+// dll named "ClonedTestSubject".  The test cases then compare the final type information of both
 // assemblies to confirm everything was re-written correction
 
 // SEE RewriteTest in Lokad.ILPack.Tests
 
 
-namespace RewriteOriginal
+namespace TestSubject
 {
     public class MyClass
     {

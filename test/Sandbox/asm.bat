34c25ef669e532a873d2dc39af308157b2de20f8
Brad Robinson <github@toptensoftware.com>
2019-05-21 18:55:42 +1000

    Added Sandbox project (#65)
    This is intended as a place to work on problem areas that currently aren't even
    stable enough to write unit tests for yet.
    
    In particular if something in the TestSubject project causes ILPack to throw an
    exception, all the unit tests will fail since the re-written assembly isn't produced.
    The idea is to work on these kinds of problems here and once working, move the test
    code into the unit tests.
    
    Other advantages to Sandbox over unit tests:
    
    *  SandboxSubject can be kept very small and targeted to a particular problem allowing
    for easier inspection of ildasm/mddumper analysis
    * Quick F5 debug/fix/re-run cycle (instead of having to select a specific unit test to run)
    * Sandbox works almost identically to the RewriteTest cases - it rewrites a test assembly
    (in this case SandboxSubject) and then invokes it with CSharpScript.
    
    Use the dump.bat script to produce ildasm and mddumper listings for original and
    cloned assemblies (requires those tools on your path).
    
    Uncomment the #define at the top of this file this to test against the original
    SandboxSubject instead of the cloned one. (to test your test case)

---
+++ test/Sandbox/asm.bat
@@ -0,0 +1,1 @@
+ilasm /dll il.clone.txt /output=bin\Debug\netcoreapp2.1\cloned\ClonedSandboxSubject.dll
---
+++ test/SandboxSubject/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SandboxSubject
+{
+    public class MyClass
+    {
+
+        public async Task<int> AsyncMethod(int x, int y)
+        {
+            await Task.Delay(100);
+            return x + y;
+        }
+    }
+}

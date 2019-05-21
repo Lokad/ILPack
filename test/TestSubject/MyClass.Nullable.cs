427f21597f881b84a63207a9fd8b7fcefbb7ffcb
Brad Robinson <contact@toptensoftware.com>
2019-05-21 23:44:40 +1000

    Unit test for nullables/PR#82

---
+++ test/TestSubject/MyClass.Nullable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// This project defines a set of types that will be rewritten by the test cases to a new
+// dll named "ClonedTestSubject".  The test cases then compare the final type information of both
+// assemblies to confirm everything was re-written correction
+
+// SEE RewriteTest in Lokad.ILPack.Tests
+
+
+namespace TestSubject
+{
+    public partial class MyClass
+    {
+        bool? _nullableBool = false;
+
+        public bool? GetNullable()
+        {
+            return _nullableBool;
+        }
+    }
+}

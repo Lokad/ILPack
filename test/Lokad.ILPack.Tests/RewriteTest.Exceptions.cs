375408db3964c84b7a50cd5acfbbb0b934e68686
Brad Robinson <contact@toptensoftware.com>
2019-05-22 09:46:35 +1000

    Test cases for Exception handling

---
+++ test/Lokad.ILPack.Tests/RewriteTest.Exceptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace Lokad.ILPack.Tests
+{
+    partial class RewriteTest
+    {
+        [Fact]
+        public async void ThrowGuardedException()
+        {
+            Assert.Equal(true, await Invoke(
+                $"var r = x.ThrowGuardedException();",
+                "r"
+                ));
+        }
+
+        [Fact]
+        public async void ThrowGuardedExceptionWithFinally()
+        {
+            Assert.Equal(0b0000_0111, await Invoke(
+                $"int r = 0; x.ThrowGuardedExceptionWithFinally(ref r);",
+                "r"
+                ));
+        }
+
+        [Fact]
+        public async void ThrowNestedGuardedExceptionWithFinally()
+        {
+            Assert.Equal(0b0111_0111, await Invoke(
+                $"int r = 0; x.ThrowNestedGuardedExceptionWithFinally(ref r);",
+                "r"
+                ));
+        }
+
+        [Fact]
+        public async void ThrowGuardedExceptionWithUntypedCatchAndFinally()
+        {
+            Assert.Equal(0b0000_0111, await Invoke(
+                $"int r = 0; x.ThrowGuardedExceptionWithUntypedCatchAndFinally(ref r);",
+                "r"
+                ));
+        }
+
+
+    }
+}
---
+++ test/TestSubject/MyClass.Exceptions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// This project defines a set of types that will be rewritten by the test cases to a new
+// dll named "ClonedTestSubject".  The test cases then compare the final type information of both
+// assemblies to confirm everything was re-written correction
+
+// SEE RewriteTest in Lokad.ILPack.Tests
+
+
+namespace TestSubject
+{
+    public partial class MyClass
+    {
+        public bool ThrowGuardedException()
+        {
+            try
+            {
+                throw new InvalidOperationException();
+            }
+            catch (InvalidOperationException)
+            {
+                return true;
+            }
+        }
+
+        public void ThrowGuardedExceptionWithFinally(ref int hitPoints)
+        {
+            try
+            {
+                hitPoints |= 0b0000_0001;
+                throw new InvalidOperationException();
+            }
+            catch (InvalidOperationException)
+            {
+                hitPoints |= 0b0000_0010;
+                return;
+            }
+            finally
+            {
+                hitPoints |= 0b0000_0100;
+            }
+        }
+
+        public void ThrowNestedGuardedExceptionWithFinally(ref int hitPoints)
+        {
+            try
+            {
+
+                hitPoints |= 0b0000_0001;
+
+                try
+                {
+                    hitPoints |= 0b0001_0000;
+                    throw new InvalidOperationException();
+                }
+                catch (InvalidOperationException)
+                {
+                    hitPoints |= 0b0010_0000;
+                    throw;
+                }
+                finally
+                {
+                    hitPoints |= 0b0100_0000;
+                }
+
+            }
+            catch (InvalidOperationException)
+            {
+                hitPoints |= 0b0000_0010;
+                return;
+            }
+            finally
+            {
+                hitPoints |= 0b0000_0100;
+            }
+        }
+
+
+        public void ThrowGuardedExceptionWithUntypedCatchAndFinally(ref int hitPoints)
+        {
+            try
+            {
+                hitPoints |= 0b0000_0001;
+                throw new InvalidOperationException();
+            }
+            catch
+            {
+                hitPoints |= 0b0000_0010;
+                return;
+            }
+            finally
+            {
+                hitPoints |= 0b0000_0100;
+            }
+        }
+
+    }
+}

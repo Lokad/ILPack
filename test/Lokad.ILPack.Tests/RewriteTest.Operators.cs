7e839728a4a36534db05ee571f8971c15fd10d90
Brad Robinson <contact@toptensoftware.com>
2019-05-22 16:58:15 +1000

    Tests for operators and casts

---
+++ test/Lokad.ILPack.Tests/RewriteTest.Operators.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace Lokad.ILPack.Tests
+{
+    partial class RewriteTest
+    {
+        [Fact]
+        public async void OperatorBinary()
+        {
+            Assert.Equal((40, 60), await Invoke(
+                $"var s = new MyStruct(10,20) + new MyStruct(30, 40);",
+                "(s.x,s.y)"));
+        }
+
+        [Fact]
+        public async void OperatorUnary()
+        {
+            Assert.Equal((-10, -20), await Invoke(
+                $"var s = -(new MyStruct(10,20));",
+                "(s.x,s.y)"));
+        }
+
+        [Fact]
+        public async void OperatorCastExplicit()
+        {
+            Assert.Equal("[10,20]", await Invoke(
+                $"string s = new MyStruct(10,20);",
+                "s"));
+        }
+
+        [Fact]
+        public async void OperatorCastImplicit()
+        {
+            Assert.Equal(5.5, await Invoke(
+                $"double r = (double)new MyStruct(1, 10);",
+                "r"));
+        }
+
+    }
+}
---
+++ test/TestSubject/MyStruct.Operators.cs
@@ -0,0 +1,36 @@
+﻿// This project defines a set of types that will be rewritten by the test cases to a new
+// dll named "ClonedTestSubject".  The test cases then compare the final type information of both
+// assemblies to confirm everything was re-written correction
+
+// SEE RewriteTest in Lokad.ILPack.Tests
+
+
+namespace TestSubject
+{
+    public partial struct MyStruct
+    {
+        // Binary operators
+        public static MyStruct operator +(MyStruct a, MyStruct b)
+        {
+            return new MyStruct(a.x + b.x, a.y + b.y);
+        }
+
+        // Unary operator
+        public static MyStruct operator -(MyStruct a)
+        {
+            return new MyStruct(-a.x, -a.y);
+        }
+
+        // Explicit operator
+        public static explicit operator double(MyStruct a)
+        {
+            return ((double)a.x + (double)a.y) / 2;
+        }
+
+        // Implicit operator
+        public static implicit operator string (MyStruct a)
+        {
+            return $"[{a.x},{a.y}]";
+        }
+    }
+}

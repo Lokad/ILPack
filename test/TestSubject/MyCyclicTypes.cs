d8b2e93a56d3d1991066d62e82b3c9965e0c817c
Joannes Vermorel <joannes.vermorel@lokad.com>
2019-05-22 09:07:38 +0200

    Failing test case on cyclic type dependencies (commented-out)
    Cyclic type dependencies occur with generic patterns such as
    `IEquatable<T>`. ILPack fails against this pattern as illustrated
    by this code snippet (courtesy of @toptensoftware).
    
    This code is intended to be uncommented when the problem is fixed.
    
    Lokad/ILPack#87

---
+++ test/TestSubject/MyCyclicTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace TestSubject
+{
+    // Illustration of circular dependencies
+
+    // TODO: [vermorel] #87, commented-out because it breaks all the rewrite unit tests.
+    // This code should be uncommented when the problem is fixed.
+
+    //public class Base<T>
+    //{
+    //}
+    //public class Derived : Base<Derived>
+    //{
+    //}
+}

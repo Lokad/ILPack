d2cd8d8030b0c63121dd9e0fee2fa3b3665c8163
Brad Robinson <contact@toptensoftware.com>
2019-05-22 16:43:33 +1000

    Test cases for "params" vararg arrays

---
+++ test/Lokad.ILPack.Tests/RewriteTest.VarArgParams.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace Lokad.ILPack.Tests
+{
+    partial class RewriteTest
+    {
+        [Fact]
+        public async void MethodWithVarArgs()
+        {
+            Assert.Equal("ApplesPearsBananas", await Invoke(
+                @"var r = x.MethodWithVarArgs(""Apples"", ""Pears"", ""Bananas"");",
+                "r"));
+        }
+
+        [Fact]
+        public async void MethodWithFixedAndVarArgs()
+        {
+            Assert.Equal("99: ApplesPearsBananas", await Invoke(
+                @"var r = x.MethodWithFixedAndVarArgs(99, ""Apples"", ""Pears"", ""Bananas"");",
+                "r"));
+        }
+
+    }
+}
---
+++ test/TestSubject/MyClass.VarArgParams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// This project defines a set of types that will be rewritten by the test cases to a new
+// dll named "ClonedTestSubject".  The test cases then compare the final type information of both
+// assemblies to confirm everything was re-written correction
+
+// SEE RewriteTest in Lokad.ILPack.Tests
+
+
+namespace TestSubject
+{
+    public partial class MyClass
+    {
+        public string MethodWithVarArgs(params string[] values)
+        {
+            return string.Join("", values);
+        }
+
+        public string MethodWithFixedAndVarArgs(int number, params string[] values)
+        {
+            return $"{number}: {string.Join("", values)}";
+        }
+    }
+}

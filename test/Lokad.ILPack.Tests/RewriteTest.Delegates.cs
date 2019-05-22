a866ef0b69f9ee88013d72897b19a6c4e12e35d8
Brad Robinson <contact@toptensoftware.com>
2019-05-22 16:33:47 +1000

    Test cases for delegates

---
+++ test/Lokad.ILPack.Tests/RewriteTest.Delegates.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace Lokad.ILPack.Tests
+{
+    partial class RewriteTest
+    {
+        [Fact]
+        public async void DelegateAction()
+        {
+            Assert.Equal(100, await Invoke(
+                $"var r = 0; x.DelegateAction(() => r = 100);",
+                "r"
+            ));
+        }
+
+        [Fact]
+        public async void DelegateActionWithParam()
+        {
+            Assert.Equal(101, await Invoke(
+                $"var r = 0; x.DelegateActionWithParam((v) => r = v, 101);",
+                "r"
+            ));
+        }
+
+        [Fact]
+        public async void DelegateFunc()
+        {
+            Assert.Equal(102, await Invoke(
+                $"var r = x.DelegateFunc(() => 102);",
+                "r"
+            ));
+        }
+
+        [Fact]
+        public async void DelegateFuncWithParam()
+        {
+            Assert.Equal(103, await Invoke(
+                $"var r = x.DelegateFuncWithParam((v) => v, 103);",
+                "r"
+            ));
+        }
+
+    }
+}
---
+++ test/TestSubject/MyClass.Delegates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// This project defines a set of types that will be rewritten by the test cases to a new
+// dll named "ClonedTestSubject".  The test cases then compare the final type information of both
+// assemblies to confirm everything was re-written correction
+
+// SEE RewriteTest in Lokad.ILPack.Tests
+
+
+namespace TestSubject
+{
+    public partial class MyClass
+    {
+        public void DelegateAction(Action cb)
+        {
+            cb();
+        }
+
+        public void DelegateActionWithParam(Action<int> cb, int val)
+        {
+            cb(val);
+        }
+
+        public int DelegateFunc(Func<int> cb)
+        {
+            return cb();
+        }
+
+        public int DelegateFuncWithParam(Func<int, int> cb, int val)
+        {
+            return cb(val);
+        }
+    }
+}

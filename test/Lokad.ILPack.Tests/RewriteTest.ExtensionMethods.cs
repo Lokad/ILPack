87696c0e84d05f3e9ca7c758c7b0cd7922add3d4
Brad Robinson <contact@toptensoftware.com>
2019-05-22 15:51:46 +1000

    Test case for extension methods

---
+++ test/Lokad.ILPack.Tests/RewriteTest.ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace Lokad.ILPack.Tests
+{
+    partial class RewriteTest
+    {
+        [Fact]
+        public async void ExtensionMethod()
+        {
+            Assert.Equal("HelloWorldHelloWorld", await Invoke(
+                $"var r = \"HelloWorld\".Repeated();",
+                "r"));
+        }
+    }
+}

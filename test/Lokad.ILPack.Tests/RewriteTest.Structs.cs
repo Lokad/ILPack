066734a1527a8fac91315d56066dfdb3a7027a66
Brad Robinson <contact@toptensoftware.com>
2019-05-23 09:13:03 +0200

    Support explicit layout structs
    Lokad/ILPack#103

--- test/Lokad.ILPack.Tests/RewriteTest.Structs.cs
+++ test/Lokad.ILPack.Tests/RewriteTest.Structs.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Runtime.InteropServices;
+using Xunit;
 
 
 namespace Lokad.ILPack.Tests
@@ -6,12 +7,31 @@ namespace Lokad.ILPack.Tests
     partial class RewriteTest
     {
         [Fact]
-        public async void BasicStructTest()
+        public async void StructBasic()
         {
             Assert.Equal((20, 30), await Invoke(
                 $"var s = x.GetMyStruct();",
                 "(s.x,s.y)"));
         }
+
+        [Fact]
+        public async void StructExplicitLayout()
+        {
+            Assert.Equal(0x1234, await Invoke(
+                $"var s = new MyExplicitLayoutStruct(); s.al=0x34; s.ah = 0x12;",
+                "(int)s.ax"
+                ));
+
+        }
+
+        [Fact]
+        public void StructExplicitLayoutSize()
+        {
+            var t = _asmCloned.GetType($"{_namespaceName}.MyExplicitLayoutStruct");
+            Assert.Equal(16, t.StructLayoutAttribute.Size);
+            Assert.Equal(1, t.StructLayoutAttribute.Pack);
+        }
+
 
     }
 }
---
+++ test/TestSubject/MyExplicitLayoutStruct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TestSubject
+{
+    [StructLayout(LayoutKind.Explicit, Size=16, Pack = 1)]
+    public struct MyExplicitLayoutStruct
+    {
+        [FieldOffset(0)] public byte al;
+        [FieldOffset(1)] public byte ah;
+        [FieldOffset(0)] public ushort ax;
+    }
+}

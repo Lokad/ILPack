3cf8e8bed5efb02a2f491ac7848fd63eff25e8d1
Brad Robinson <contact@toptensoftware.com>
2019-05-23 11:42:53 +1000

    Test cases for implementing imported interfaces and imported generic interfaces

---
+++ test/TestSubject/MyComparableT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSubject
+{
+    public class MyComparableT : IComparable<MyComparableT>
+    {
+        public MyComparableT(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        public int CompareTo(MyComparableT obj)
+        {
+            if (obj == null) return 1;
+
+            return obj.Value.CompareTo(this.Value);
+        }
+
+    }
+}

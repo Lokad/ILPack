6d4c2209b17f5fdb1e79bec92368bae85b9c445a
Mason Wheeler <masonwheeler@yahoo.com>
2021-12-10 11:11:08 -0700

    Adding tests for the recent changes

---
+++ test/TestSubject/MyClass.ConstructedGenerics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSubject
+{
+    public struct GenericClass<T>
+    {
+        public T Value;
+    }
+
+    public partial class MyClass
+    {
+        private GenericClass<int> _genericInt = new() { Value = 5 };
+
+        public GenericClass<int> GenericInt => _genericInt;
+
+        public GenericClass<T> GenericMethod<T>(T value)
+        {
+            return new() { Value = value };
+        }
+    }
+}
